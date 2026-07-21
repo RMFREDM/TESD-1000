fa65938df225ae98d3b4e6bfc19e9b507f57191b
Web Development <rmfredm@stu.ccsdut.org>
2026-07-21 09:10:07 -0600

    Completed mutation challenge
    The mutation challenge correctly changes the position and color of the box without moving the background. The background continuously overlapped the select option, unlike in the online starter code, so this was fixed by manually setting the select's position.

--- app/interactivity-code-along/mutation/page.jsx
+++ app/interactivity-code-along/mutation/page.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Background from "./Background.js";
 import Box from "./Box.js";
+import { transform } from "typescript";
 
 const initialPosition = {
 	x: 0,
@@ -16,8 +17,15 @@ export default function Canvas() {
 	});
 
 	function handleMove(dx, dy) {
-		shape.position.x += dx;
-		shape.position.y += dy;
+		const nextShape = {
+			...shape,
+			position: {
+				x: shape.position.x + dx,
+				y: shape.position.y + dy,
+			},
+		};
+
+		setShape(nextShape);
 	}
 
 	function handleColorChange(e) {
@@ -29,7 +37,15 @@ export default function Canvas() {
 
 	return (
 		<>
-			<select value={shape.color} onChange={handleColorChange}>
+			<select
+				value={shape.color}
+				onChange={handleColorChange}
+				style={{
+					// the position is set because the background covered the select
+					position: "absolute",
+					transform: "translate(0px, -20px)",
+				}}
+			>
 				<option value="orange">orange</option>
 				<option value="lightpink">lightpink</option>
 				<option value="aliceblue">aliceblue</option>
