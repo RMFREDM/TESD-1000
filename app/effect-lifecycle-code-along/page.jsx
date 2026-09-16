9e5966540ea854c7c1c44e1701e8c4fa563d3c19
Web Development <rmfredm@stu.ccsdut.org>
2026-09-16 09:37:41 -0600

    Correct page headers
    Corrected the page headers in each route to accurately reflect their content.

--- app/effect-lifecycle-code-along/page.jsx
+++ app/effect-lifecycle-code-along/page.jsx
@@ -4,8 +4,8 @@ export default function Home() {
 			<main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
 				<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
 					<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-						This page contains links to the challenges in the
-						useEffect Code-Along.
+						This page contains links to the challenges in the Effect
+						Lifecycle Code-Along.
 					</h1>
 
 					<ul>
